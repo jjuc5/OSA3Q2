24d5d729b239c6d6e97c8e82145343e4de5a5d1e
melan <melan@DESKTOP-U2KOML6>
2017-12-09 17:30:25 -0500

    Completion of EchoClient

--- src/osa3q2/EchoClient.java
+++ src/osa3q2/EchoClient.java
@@ -4,33 +4,29 @@
     Assignment: Assignment 3 - Q2
     Authors: Melanie Iarocci, John Urbanowicz
     Date: 12/09/2017
-*/
-
+ */
 package osa3q2;
 
 import java.io.*;
 import java.net.*;
 
 /**
- * This class represents the client.  It allows one to connect to the echo server
- * and to take input from the user, outputting it towards the Echo server.  User 
- * may also choose to sever the connection by inputting "exit".  To run more clients,
- * please run more instances of this file.
- * 
+ * This class represents the client. It allows one to connect to the echo server
+ * and to take input from the user, outputting it towards the Echo server. User
+ * may also choose to sever the connection by inputting "exit". To run more
+ * clients, please run more instances of this file.
+ *
  * @author Melanie Iarocci, John Urbanowicz
  */
-public class EchoClient extends Thread
-{
+public class EchoClient extends Thread {
 
     /**
      * The main method for the client class.
-     * 
+     *
      * @param args
      */
-    public static void main(String[] args)
-    {
-        try
-        {
+    public static void main(String[] args) {
+        try {
             // Establishes a socket with loopback address selected and port 9999
             // Sets up input readers and output stream to send data to Echo Server
             Socket s = new Socket("localhost", 9999);
@@ -38,13 +34,22 @@ public class EchoClient extends Thread
             PrintWriter pw = new PrintWriter(s.getOutputStream(), true);
             BufferedReader con = new BufferedReader(new InputStreamReader(System.in));
             String line;
-        
-            
-            
-        }
-        catch(Exception ex)
-        {
-            
+            // While the user has not inputted the exit clause to close connection,
+            // the loop will continually query the user for input
+            do {
+                line = br.readLine();
+                if (line != null) {
+                    if (line.equals("exit")) {
+                        s.close();
+                    }
+                    System.out.println(line);
+                }
+                line = con.readLine();
+                pw.println(line);
+            } while (!line.trim().equals("exit"));
+
+        } catch (IOException ex) {
+            System.err.println(ex);
         }
     }
 }
