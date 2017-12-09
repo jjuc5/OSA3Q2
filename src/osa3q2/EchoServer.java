cc5f8100a5ad65f18c4a59d9f947ad97f39a016e
urban <urban@JOHN-LAPTOP>
2017-12-09 17:19:43 -0500

    Partial Server

--- src/osa3q2/EchoServer.java
+++ src/osa3q2/EchoServer.java
@@ -1,23 +1,79 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    Course: OS Design & Systems Programming
+    Professor: Cristina Ribeiro
+    Assignment: Assignment 3 - Q2
+    Authors: Melanie Iarocci, John Urbanowicz
+    Date: 12/09/2017
+*/
+
 package osa3q2;
 
-/**
- *
- * @author urban
- */
-public class EchoServer
-{
+import java.io.*;
+import java.net.*;
 
+/**
+ * This class represents a multi-threaded Echo Server.  It accepts client connection and waits
+ * for user input.  Upon receipt, it returns the value received from the client.
+ * It also detects proper closure of the client connection.  It can accept connections
+ * from multiple clients concurrently.
+ * 
+ * @author Melanie Iarocci, John Urbanowicz
+ */
+public class EchoServer implements Runnable
+{
+    Socket clientSocket;
+    public static ServerSocket serverSocket;
+    
+    public EchoServer(Socket clientSocket)
+    {
+        this.clientSocket = clientSocket;
+    }
+    
     /**
-     * @param args the command line arguments
+     * This is the main method for the EchoServer class.
+     * 
+     * @param args 
      */
     public static void main(String[] args)
     {
-        // TODO code application logic here
+        serverSocket = null;
+        EchoServer s = new EchoServer(9999);
     }
     
+    /**
+     * EchoServer method creates and instantiates a serverSocket socket.  This will
+     * create new server sockets for each client that attempts to connect.
+     *
+     * @param portnum the port number argument passed
+     */
+    public EchoServer(int portnum)
+    {
+        try
+        {
+            serverSocket = new ServerSocket(portnum);
+            
+            int threadCount = 1;
+            
+            while(true)
+            {
+                Socket newSock = serverSocket.accept();
+                System.out.println("New client connected.");
+                Runnable newRunnable = new EchoServer(newSock);
+                Thread newThread = new Thread(newRunnable);
+                newThread.start();
+                System.out.println("Thread: " + threadCount);
+                threadCount++;
+            }
+        }
+        catch (Exception ex)
+        {
+            System.out.println(ex);
+        }
+    }
+
+    @Override
+    public void run()
+    {
+    }
+
 }
